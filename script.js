cc25844078f69e8ed6e09340ff711c22fd3769e4
EnmanuelJS <enmanuelsegura859@gmail.com>
2024-11-06 18:12:02 -0400

    agregando funcion de cambio de imagen a botones de descanso y enfoque

--- script.js
+++ script.js
@@ -3,17 +3,28 @@ const botonEnfoque = document.querySelector('.app__card-button--enfoque');
 const botonDescansoCorto = document.querySelector('.app__card-button--corto');
 const botonDescansoLargo = document.querySelector('.app__card-button--largo');
 
+// funcion de cambio de imagen
+const cambiarImagen = (rutaImagen) => { 
+   let imagen = document.querySelector('.app__image');
+   imagen.setAttribute('src',rutaImagen);
+
+}
+
 
 botonEnfoque.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'enfoque');
+    cambiarImagen('./imagenes/enfoque.png')
+
 })
 
 botonDescansoCorto.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-corto');
+    cambiarImagen('./imagenes/descanso-corto.png')
 
 })
 
 botonDescansoLargo.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-largo');
+    cambiarImagen('./imagenes/descanso-largo.png')
 })
 
