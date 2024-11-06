b6cb23656b642e7145d105f4f874f48282851f95
EnmanuelJS <enmanuelsegura859@gmail.com>
2024-11-05 21:07:43 -0400

    se agrego funcionalidad a los botones (ahora cambia de color el fondo por boton)

---
+++ script.js
@@ -0,0 +1,19 @@
+const html = document.querySelector('html');
+const botonEnfoque = document.querySelector('.app__card-button--enfoque');
+const botonDescansoCorto = document.querySelector('.app__card-button--corto');
+const botonDescansoLargo = document.querySelector('.app__card-button--largo');
+
+
+botonEnfoque.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'enfoque');
+})
+
+botonDescansoCorto.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'descanso-corto');
+    
+})
+
+botonDescansoLargo.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'descanso-largo');
+})
+
