6260b6e461e20f5b39bcccceee1de141c246f880
EnmanuelJS <enmanuelsegura859@gmail.com>
2024-11-07 11:33:57 -0400

    los botones cambia el fondo cuando se clican

--- script.js
+++ script.js
@@ -3,12 +3,18 @@ const botonEnfoque = document.querySelector('.app__card-button--enfoque');
 const botonDescansoCorto = document.querySelector('.app__card-button--corto');
 const botonDescansoLargo = document.querySelector('.app__card-button--largo');
 const titulo = document.querySelector('.app__title');
+const botones = document.querySelectorAll('.app__card-button');
 
 // funcion de cambio de contexto
 const cambiarContexto = (contexto) => { 
    let imagen = document.querySelector('.app__image');
       imagen.setAttribute('src',`./imagenes/${contexto}.png`);
       html.setAttribute('data-contexto',contexto);
+      botones.forEach(contexto => {
+         contexto.classList.remove('active');
+      });
+      
+
       // cambio de texto segun contexto
       switch (contexto) {
          case 'enfoque': 
@@ -29,20 +35,25 @@ const cambiarContexto = (contexto) => {
                 <strong class="app__title-strong" >Haz una pausa larga</strong>
                `
             break;
-      }
+         }
 
 }
 
 
 botonEnfoque.addEventListener('click', () => {
    cambiarContexto('enfoque');
+   botonEnfoque.classList.add('active');
+   
 })
 
 botonDescansoCorto.addEventListener('click', () => {
     cambiarContexto('descanso-corto');
+    botonDescansoCorto.classList.add('active');
 
 })
 
 botonDescansoLargo.addEventListener('click', () => {
    cambiarContexto('descanso-largo');
+   botonDescansoLargo.classList.add('active');
+
 })
