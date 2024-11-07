8bb33a8a03e6930efe8d4fdb8798337016355074
EnmanuelJS <enmanuelsegura859@gmail.com>
2024-11-07 09:58:37 -0400

    el texto se actualiza al clicar los botones de descanso

--- script.js
+++ script.js
@@ -2,12 +2,34 @@ const html = document.querySelector('html');
 const botonEnfoque = document.querySelector('.app__card-button--enfoque');
 const botonDescansoCorto = document.querySelector('.app__card-button--corto');
 const botonDescansoLargo = document.querySelector('.app__card-button--largo');
+const titulo = document.querySelector('.app__title');
 
-// funcion de cambio de imagen
+// funcion de cambio de contexto
 const cambiarContexto = (contexto) => { 
    let imagen = document.querySelector('.app__image');
       imagen.setAttribute('src',`./imagenes/${contexto}.png`);
       html.setAttribute('data-contexto',contexto);
+      // cambio de texto segun contexto
+      switch (contexto) {
+         case 'enfoque': 
+               titulo.innerHTML = `
+                    Optimiza tu productividad,<br>
+                  <strong class="app__title-strong" >sumérgete en lo que importa.</strong>
+               `
+            break;
+         case 'descanso-corto':
+               titulo.innerHTML = `
+                  Toma un respiro,<br>
+                <strong class="app__title-strong" >Haz una pausa corta</strong>
+               `
+            break;
+         case 'descanso-largo':
+               titulo.innerHTML = `
+                  Hora de volver a la superficie,<br>
+                <strong class="app__title-strong" >Haz una pausa larga</strong>
+               `
+            break;
+      }
 
 }
 
